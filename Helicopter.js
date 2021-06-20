f228cad02f30f2cb3caf5c532508521824eae382
PihuB14 <79080134+PihuB14@users.noreply.github.com>
2021-06-20 10:20:41 +0530

    Add files via upload

---
+++ Helicopter.js
@@ -0,0 +1,15 @@
+class Helicopter extends BaseClass{
+    constructor(x,y){
+        super(x,y,50,50);
+        this.image = loadImage("sprites/helicopterSprite.png");
+    }
+    display(){
+        var pos= this.body.position;
+        pos.x = mouseX;
+        pos.y = mouse.Y;
+
+
+        super.display();
+
+    };
+}
---
+++ Package.js
@@ -0,0 +1,6 @@
+class Package extends BaseClass{
+    contructor(x,y,width,height){
+        super(x,y,width,height)
+        this.image = loadImage("sprites/package.png")
+    }
+}
